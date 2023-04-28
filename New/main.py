c14d8a9417c0acc0a692cd89d8314f0c40d774c3
AdityaPathak09 <pathakaditya1653@gmail.com>
2023-04-28 16:15:09 +0530

    All Codes Commit 28th April 2023
    Backend algos and frontend is committed at once.

---
+++ New/main.py
@@ -0,0 +1,8 @@
+
+import sys
+    # caution: path[0] is reserved for script path (or '' in REPL)
+sys.path.insert(1, '/home/pi/Aditya_Work/backend_algorithms')
+
+import print_text
+
+print_text.print_someting()
